090a0dc975f97b61c86d03fe6a49327a286081a7
EZ118 <79049368+EZ118@users.noreply.github.com>
2022-02-04 09:31:37 +0800

    Update reader.js

--- js/reader.js
+++ js/reader.js
@@ -2,18 +2,18 @@ function reader_open(aid){
 	var w = document.documentElement.clientWidth || document.body.clientWidth;
 	var h = document.documentElement.clientHeight || document.body.clientHeight;
 	
-	document.getElementById("reader_backcover").style.display = "block";
-	document.getElementById("reader_outbox").style.display = "block";
 	document.getElementById("reader_frame").src = "./view.html?id=" + aid;
 	document.getElementById("reader_frame").width = w * 0.7 - 10 - 7;
 	document.getElementById("reader_frame").height = h * 0.8 - 10 - 20;
 	document.getElementById("reader_title").innerHTML = "文章ID: " + aid;
+	document.getElementById("reader_backcover").style.display = "block";
+	document.getElementById("reader_outbox").style.display = "block";
 }
 
 function reader_close(){
 	document.getElementById("reader_backcover").style.display = "none";
 	document.getElementById("reader_outbox").style.display = "none";
-	document.getElementById("reader_frame").src = "./view.asp?id=";
+	document.getElementById("reader_frame").src = "./view.html?id=";
 }
 
 function writer_open(){
