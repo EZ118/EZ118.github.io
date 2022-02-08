0d13d7c91a7cb51fd94c8108c7c5e95abbe083d6
EZ118 <79049368+EZ118@users.noreply.github.com>
2022-02-08 13:45:57 +0800

    Update load.js

--- js/load.js
+++ js/load.js
@@ -27,3 +27,13 @@ function loadlist() {
 	xhttp.open("GET", "info.txt", true);
 	xhttp.send(); 
 }
+
+function $GET(name){
+	var reg = new RegExp('(^|&)'+name+'=([^&]*)(&|$)', 'i');
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null)
+	{
+		return unescape(r[2]);
+	}
+	return "";
+}
